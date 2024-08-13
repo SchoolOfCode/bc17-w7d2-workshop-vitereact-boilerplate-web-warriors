8e675aae259e20192e9b46c01ef7521a32833189
Mahik <mahik09@outlook.com>
2024-08-13 14:03:02 +0100

    Updated MainSection function

--- src/Components/MainSection/MainSection.jsx
+++ src/Components/MainSection/MainSection.jsx
@@ -3,6 +3,18 @@ import "./MainSection.css";
 
 // One function for hero section
 
+function Hero() {
+  <section className="section hero hero-section">
+    <img src="Images/hero-mobile.png" alt="" />
+    <article>
+      <h1>Discover the perfect Fireplace..</h1>
+      <h2>
+        Book consultation: <span>0121 345 6789</span>
+      </h2>
+    </article>
+  </section>;
+}
+
 // One function for the how it works section
 
 //  export those two into main.jsx
