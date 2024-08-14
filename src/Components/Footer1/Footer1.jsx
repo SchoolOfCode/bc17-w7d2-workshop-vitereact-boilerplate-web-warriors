633b71d42b55f5234c7d655a7ed6eee8071d6453
Adil <adilk23@outlook.com>
2024-08-14 13:47:03 +0100

    fix footer

---
+++ src/Components/Footer1/Footer1.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import styles from "./Footer1.module.css";
+
+function Footer() {
+  return (
+    <footer>
+    <div>
+      <p>Find us on:</p>
+      <ul>
+        <li>Facebook</li>
+        <li>Instagram</li>
+        <li>TikTok</li>
+      </ul>
+    </div>
+    <div>
+      <p>©️ Fireplace Palace</p>
+      <p><a href="mailto:info@firepalace.co.uk">info@firepalace.co.uk</a></p>
+    </div>
+  </footer>
+  );
+}
+
+export default Footer;
+
--- src/Components/Wrapper/Wrapper.jsx
+++ src/Components/Wrapper/Wrapper.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./Wrapper.css";
 import Header from "../Header/Header.jsx";
+import Footer from "../Footer1/Footer1.jsx";
+
+
 
 function Wrapper() {
   return (
     <div className="wrapper">
       <Header></Header>
-
+      <h1>Home page</h1>
+      <Footer></Footer>
     </div>
   );
 }
